2f47fa64f8b858c91fb7bcb8d2860bfe5e3f1043
DIYdy <1327990317@qq.com>
2022-03-24 21:48:01 +0800

    发布视频完成

--- src/components/releaseVideo/ContentMessage.vue
+++ src/components/releaseVideo/ContentMessage.vue
@@ -1,22 +1,80 @@
 <template>
   <div class="contentMessage">
     <div class="cm_title">视频管理</div>
+    <div class="cm_tab">
+      <div class="cm_tab_item" @click="changeBorder($event, 0)">全部作品</div>
+      <div class="cm_tab_item" @click="changeBorder($event, 1)">已发布</div>
+      <div class="cm_tab_item" @click="changeBorder($event, 2)">审核中</div>
+      <div class="cm_tab_item" @click="changeBorder($event, 3)">未通过</div>
+      <div class="cm_border cm_border_hover"></div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "ContentMessage",
+  methods: {
+    //改变border位置
+    changeBorder(e, n) {
+      let bor = document.querySelector(".cm_border_hover");
+      bor.style.width = "3rem";
+      if(n == 0) {
+        bor.style.width = "4rem";
+        bor.style.transform = `translateX(0px)`
+      }else if(n == 1) {
+        bor.style.transform = `translateX(${96}px)`
+      }else if(n == 2) {
+        bor.style.transform = `translateX(${96 + 80}px)`
+      }else if(n == 3) {
+        bor.style.transform = `translateX(${96 * 2 + 64}px)`
+      }
+    },
+  }
 };
 </script>
 
 <style lang="scss" scoped>
 .contentMessage {
   user-select: none;
+  position: relative;
   .cm_title {
     font-weight: bold;
     font-size: 1.5rem;
     text-align: left;
   }
+  .cm_tab {
+    display: flex;
+    position: relative;
+    margin: {
+      top: 1rem;
+    };
+    padding: {
+      bottom: 1rem;
+    }
+    border: {
+      bottom: 0.01rem solid #b2b2b2;
+    }
+    .cm_tab_item {
+      margin-right: 2rem;
+      font-weight: bold;
+      cursor: pointer;
+    }
+    .cm_border {
+      position: absolute;
+      width: 4rem;
+      height: 0.1rem;
+      background-color: red;
+      bottom: 0;
+      transition: all 0.5s ease-in-out;
+    }
+  }
+}
+</style>
+
+<style lang="scss">
+.cm_border_hover {
+  width: 4rem;
+  transform: translateX(0);
 }
 </style>
--- src/views/UploadVideo/UploadVideoRight.vue
+++ src/views/UploadVideo/UploadVideoRight.vue
@@ -1,12 +1,65 @@
 <template>
   <div class="uploadVideoRight">
     <div class="uvr_title">发布视频</div>
+    <div class="uvr_lunbo_box">
+      <el-carousel :interval="5000" arrow="always" height="80px">
+        <el-carousel-item v-for="item in advert" :key="item.id">
+          <img :src="item.src" class="uvr_lunbp_img" />
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+    <div class="uvr_upload_box">
+      <el-upload
+        class="upload-demo"
+        drag
+        action="https://jsonplaceholder.typicode.com/posts/"
+        multiple
+      >
+        <i class="el-icon-upload"></i>
+        <div class="el-upload__text">将视频拖到此处，或<em>点击上传</em></div>
+        <div class="el-upload__tip" slot="tip">
+          为了更好的观看体验，超过40秒的视频建议上传横版视频
+        </div>
+      </el-upload>
+    </div>
+    <div class="uvr_video_format">
+      <div class="uvr_video_proposal">
+        <div class="uvrvp_lt">视频格式</div>
+        <div class="uvrvp_st">支持常用视频格式，推荐使用mp4、webm</div>
+      </div>
+      <div class="uvr_video_proposal">
+        <div class="uvrvp_lt">视频大小</div>
+        <div class="uvrvp_st">视频文件大小不超过8G，时长在30分钟以内</div>
+      </div>
+      <div class="uvr_video_proposal">
+        <div class="uvrvp_lt">视频分辨率</div>
+        <div class="uvrvp_st">分辨率为720p（1280x720）及以上</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "UploadVideoRight",
+  data() {
+    return {
+      advert: [
+        {
+          src: require("../../assets/images/advertImg/advert01.jpeg"),
+          id: "jjgj",
+        },
+        {
+          src: require("../../assets/images/advertImg/advert02.jpeg"),
+          id: "sr",
+        },
+        {
+          src: require("../../assets/images/advertImg/advert03.jpeg"),
+          id: "dv",
+        },
+      ],
+    };
+  },
 };
 </script>
 
@@ -17,6 +70,49 @@ export default {
     font-weight: bold;
     font-size: 1.5rem;
     text-align: left;
+  }
+  .uvr_lunbo_box {
+    margin-top: 2rem;
+    .el-carousel__item img {
+      height: 5rem;
+      margin: 0;
+    }
+  }
+  .uvr_upload_box {
+    margin-top: 1rem;
+    height: 20rem;
+    background-color: rgba(244, 245, 245, 1);
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
+  .uvr_video_format {
+    display: flex;
+    justify-content: space-between;
+    margin: {
+      top: 1rem;
+    }
+    .uvr_video_proposal {
+      background-color: rgba(244, 245, 245, 1);
+      text-align: left;
+      padding: 1rem 1rem;
+      .uvrvp_lt {
+        font-size: 0.9rem;
+        font-weight: bold;
+      }
+      .uvrvp_st {
+        font-size: 0.8rem;
+        color: rgba(156, 159, 162, 1);
+      }
+    }
+  }
+}
+</style>
+
+<style lang="scss">
+.uvr_upload_box {
+  .el-upload-dragger {
+    background-color: rgba(244, 245, 245, 1);
   }
 }
 </style>
